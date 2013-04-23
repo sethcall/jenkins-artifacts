2b0fa820b9044e971852ecfef98b2864e43a0dfc
Seth Call <sethcall@gmail.com>
2013-04-23 08:58:56 -0500

    * make it so that the order doesn't change per showing of the page

--- js/main.js
+++ js/main.js
@@ -15,23 +15,34 @@
         var currentRowElement = null;
         var count = 0;
 
+        // lay down skeleton of page before responses of any REST API's are received,
+        // so that the order of the page is deterministic
         $.each(jenkins.projects, function(index, project) {
+            var currentRow = Math.floor(count / itemsPerRow);
+            count ++;
+
+            // determine if we need to add a new bootstrap row
+            if (currentRow > lastRow) {
+                lastRow = currentRow
+                currentRowElement = $('<div class="row"></div>')
+                $('footer').before(currentRowElement)
+            }
+
+            // create a div to old this project's build artifacts and details
+            var item = $('<div class="span4" id="' + project.name + '"></div>');
+
+            // add item to the current row
+            currentRowElement.append(item)
+        })
+
+        // now fetch data to populate skeleton of page
+        $.each(jenkins.projects, function(index, project) {
+
             context.jenkins.rest.getBuildInfo(project.name, version)
                 .done(function(data) {
 
-                    var currentRow = Math.floor(count / itemsPerRow);
-                    count ++;
+                    var item = $('#' + project.name)
 
-                    // determine if we need to add a new bootstrap row
-                    if (currentRow > lastRow) {
-                        lastRow = currentRow
-                        currentRowElement = $('<div class="row"></div>')
-                        $('.hero-unit').after(currentRowElement)
-                    }
-
-
-                    // create a div to old this project's build artifacts and details
-                    var item = $('<div class="span4"></div>');
                     item.append($('<h2></h2>').append($('<a></a>').attr('href', jenkins.rest.projectPath(project.name)).text(project.name)))
 
                     var details = $("<dl class='dl-horizontal'></dl>")
@@ -53,8 +64,6 @@
 
                     item.append(details)
 
-                    // add item to the current row
-                    currentRowElement.append(item)
                 })
                 .fail(function(data) {
                     console.log("unable to fetch build info for %o %o: %o", project.name, 'lastSuccessfulBuild', arguments)
